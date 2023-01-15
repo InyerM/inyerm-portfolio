c1ab72d1e9349f42fa9daf5c5b4869730ca88644
Inyer Marín Medina <inyermedina10@gmail.com>
2023-01-14 22:27:25 -0500

    UPDATE: add i18n react server pluggins

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -6,6 +6,15 @@ const config: AstroI18nextConfig = {
   i18nextServer: {
     debug: true,
   },
+  i18nextClient: {
+    debug: true,
+  },
+  i18nextServerPlugins: {
+    "{initReactI18next}": "react-i18next",
+  },
+  i18nextClientPlugins: {
+    "{initReactI18next}": "react-i18next",
+  },
 };
 
 export default config;
