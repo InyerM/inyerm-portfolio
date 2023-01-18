31aacf8ed9d852ca31bd3938c4da2196bfd80bdb
Inyer <inyermedina10@gmail.com>
2023-01-17 21:00:31 -0500

    UPDATE: card links

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,5 @@
-import "../styles/Button.css";
-import "../styles/Card.css";
-import "../styles/NavButton.css";
-import "../styles/globals.css";
+import "./Button.css";
+import "./Card.css";
+import "./CardLink.css";
+import "./globals.css";
+import "./NavButton.css";
